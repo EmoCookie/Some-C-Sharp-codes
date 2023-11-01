5b189c7e51685144d4a39e8180e26e2a8471db57
EmoCookie <129533088+EmoCookie@users.noreply.github.com>
2023-11-01 16:45:18 +0530

    Add files via upload
    A very simple game where the user has to guess a number between 1 - 100.
    
    Credit : @Bro Code

---
+++ GuessNumber/GuessNumber/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace GuessGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            bool playAgain = true;
+            int min = 1;
+            int max = 100;
+            int guess;
+            int num;
+            int guesses;
+            String response;
+
+            while (playAgain)
+            {
+                guess = 0;
+                guesses = 0;
+                response = "";
+                num = random.Next(min, max + 1);
+
+                while (guess != num)
+                {
+                    Console.WriteLine("Guess a number between " + min + " - " + max + " : ");
+                    guess = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Guess: " + guess);
+
+                    if (guess > num)
+                    {
+                        Console.WriteLine(guess + " is too high!");
+                    }
+                    else if (guess < num)
+                    {
+                        Console.WriteLine(guess + " is too low!");
+                    }
+                    guesses++;
+                }
+                Console.WriteLine("Number: " + num);
+                Console.WriteLine("You win!");
+                Console.WriteLine("Guesses: " + guesses);
+
+                Console.WriteLine("Would you like to play again (Y/N)? ");
+                response = Console.ReadLine();
+                response = response.ToUpper();
+
+                if (response == "Y")
+                {
+                    playAgain = true;
+                }
+                else
+                {
+                    playAgain = false;
+                }
+            }
+
+            Console.WriteLine("Thanks for playing!");
+
+            Console.ReadKey();
+
+        }
+    }
+}
